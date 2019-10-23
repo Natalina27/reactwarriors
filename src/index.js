ac19441f0f47d9f4759c3e2f7639e3b4afee05ea
Natalya <natalyamyunster@yahoo.com>
2019-10-23 17:06:31 +0200

    added App and MovieItem functions

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ function MovieItem(props) {
             <p>{props.title}</p>
             <p>{props.vote_average}</p>
         </div>
-    )
+    );
 }
 function App(){
     return (
     <div>
         <MovieItem title="Title new" vote_average={10.1} />
     </div>
-    )
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
